8fccd1f3a63d83d17f5a022a646f54a600d7fbd0
Ahmed Radwan <aradwann@proton.me>
2022-08-30 22:43:49 +0200

    handle isLoading and Error state in data fetching

--- src/components/workspaces/WorkspaceList.tsx
+++ src/components/workspaces/WorkspaceList.tsx
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import API from "../../api/api";
 import { Workspace } from "./workspace.interface";
@@ -5,11 +6,20 @@ import WorkspaceListItem from "./WorkspaceListItem";
 
 export default function WorkspaceList() {
   const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const fetchData = async () => {
-    const response = await API.get<Workspace[]>("/workspaces/me");
-    const workspaces = response.data;
-    setWorkspaces(workspaces);
-    console.log(workspaces);
+    setIsLoading(true);
+    try {
+      const response = await API.get<Workspace[]>("/workspaces/me");
+      const workspaces = response.data;
+      setWorkspaces(workspaces);
+    } catch (error: any) {
+      setError(error.response.data.message);
+    }
+
+    setIsLoading(false);
   };
   useEffect(() => {
     fetchData();
@@ -22,9 +32,21 @@ export default function WorkspaceList() {
       description={workspace.description}
     />
   ));
+
   return (
-    <ul className="flex flex-col w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
-      {workspacesListItems}
-    </ul>
+    <div className="flex flex-col justify-center w-full">
+      <button onClick={fetchData} className="bg-green-800 rounded-lg">
+        Refresh
+      </button>
+      {isLoading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h1>{error}</h1>
+      ) : (
+        <ul className="flex flex-col w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+          {workspacesListItems}
+        </ul>
+      )}
+    </div>
   );
 }
