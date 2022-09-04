acab32f4290545ae1ec84763a4771d0d029edac8
Ahmed Radwan <aradwann@proton.me>
2022-09-04 14:15:06 +0200

    separate joined and created workspaces

--- src/components/workspaces/WorkspaceList.tsx
+++ src/components/workspaces/WorkspaceList.tsx
@@ -1,30 +1,60 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useDataApi } from "../../api/hooks/useDataApi";
 import { Workspace } from "./workspace.interface";
 import WorkspaceListItem from "./WorkspaceListItem";
 
 export default function WorkspaceList() {
-  const [state, fetchData] = useDataApi<Workspace[]>([], "/workspaces/me");
+  const [joinedWorkspacesState, fetchJoinedWorkspaces] = useDataApi<
+    Workspace[]
+  >([], "/workspaces/joined");
+  const [createdWorkspacesState, fetchCreatedWorkspaces] = useDataApi<
+    Workspace[]
+  >([], "/workspaces/created");
 
-  const workspacesListItems = state.data.map((workspace) => (
-    <WorkspaceListItem {...workspace} key={workspace.id} />
-  ));
+  const joinedWorkspacesListItems = joinedWorkspacesState.data.map(
+    (workspace) => <WorkspaceListItem {...workspace} key={workspace.id} />
+  );
+  const createdWorkspacesListItems = createdWorkspacesState.data.map(
+    (workspace) => <WorkspaceListItem {...workspace} key={workspace.id} />
+  );
 
   return (
     <div className="flex flex-col justify-center w-full">
-      <button onClick={fetchData} className="bg-green-800 rounded-lg">
-        Refresh
-      </button>
-      {state.isLoading ? (
-        <h1>Loading</h1>
-      ) : state.error ? (
-        <h1>{state.error}</h1>
-      ) : (
-        <ul className="flex flex-col w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
-          {workspacesListItems}
-        </ul>
-      )}
+      <div className="flex flex-col justify-center w-full">
+        <h2 className="flex flex-row justify-center text-3xl">Created Workspaces</h2>
+        <button
+          onClick={fetchCreatedWorkspaces}
+          className="bg-green-800 rounded-lg"
+        >
+          Refresh
+        </button>
+        {createdWorkspacesState.isLoading ? (
+          <h1>Loading</h1>
+        ) : createdWorkspacesState.error ? (
+          <h1>{createdWorkspacesState.error}</h1>
+        ) : (
+          <ul className="flex flex-col w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+            {createdWorkspacesListItems}
+          </ul>
+        )}
+      </div>
+      <div className="flex flex-col justify-center w-full">
+        <h2 className="flex flex-row justify-center text-3xl">Joined Workspaces</h2>
+        <button
+          onClick={fetchJoinedWorkspaces}
+          className="bg-green-800 rounded-lg"
+        >
+          Refresh
+        </button>
+        {joinedWorkspacesState.isLoading ? (
+          <h1>Loading</h1>
+        ) : joinedWorkspacesState.error ? (
+          <h1>{joinedWorkspacesState.error}</h1>
+        ) : (
+          <ul className="flex flex-col w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+            {joinedWorkspacesListItems}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/workspaces/WorkspacesPage.tsx
+++ src/components/workspaces/WorkspacesPage.tsx
@@ -8,10 +8,10 @@ export default function WorkspacesPage() {
   };
   return (
     <div className="flex flex-col">
-      <h1 className="flex flex-row justify-center">
+      <h1 className="flex flex-row justify-center text-5xl">
         Welcome to your workspaces
       </h1>
-      <div className="flex flex-row justify-center w-full">
+      <div className="flex flex-row justify-center w-full p-4">
         <button
           type="button"
           className=" text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
