beadbf0d8c1c98d6b7f832f7348e800df214cf65
Ahmed Radwan <aradwann@proton.me>
2022-09-05 14:43:53 +0200

    fix refreshing the page cause axios token cleared

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,9 @@ const API = axios.create({
   baseURL: `http://localhost:8000`,
 });
 
+API.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
+  "jwt"
+)}`;
 // axios.interceptors.request.use(function (config: any) {
 //   const token = localStorage.getItem("jwt");
 //   config.headers.Authorization = token;
